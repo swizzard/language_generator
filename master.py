df8ff3ee8de6b0116b262f0e0937f3a1f33d4001
Sam Raker <sam.raker@gmail.com>
2014-05-23 15:02:48 -0400

    breaking things up into separate files

---
+++ master.py
@@ -0,0 +1,103 @@
+from morpheme_generator import MorphemeGeneratorMixin
+from phono import Phonology
+from morphology import Nominal, Verbal, Other
+from syntax import Syntax
+
+class Master(MorphemeGeneratorMixin):
+    def __init__(self, phonological_adjustments=None, syntax_adjustments=None, nominal_adjustments=None,
+                 verbal_adjustments=None, other_adjustments=None, phonological_likelihoods=None,
+                 syntax_likelihoods=None, nominal_likelihoods=None, verbal_likelihoods=None,
+                 other_likelihoods=None):
+        self.phonological_instance = Phonology(phonological_likelihoods, phonological_adjustments)
+        self.nominal_instance = Nominal(self.phonological_instance.inventory, nominal_likelihoods, nominal_adjustments)
+        self.verbal_instance = Verbal(self.phonological_instance.inventory, self.nominal_instance, verbal_likelihoods,
+                                      verbal_adjustments)
+        self.other_instance = Other(self.phonological_instance.inventory, self.nominal_instance, other_likelihoods,
+                                    other_adjustments)
+        self.syntax_instance = Syntax(self.nominal_instance, self.verbal_instance, self.other_instance,
+                                      syntax_likelihoods, syntax_adjustments)
+        self.inventory = {}
+        self.lexicon = {}
+
+    def gen_lexical_element(self, word, pos=None, syll='cv'):
+        if word not in self.inventory:
+            self.gen_morpheme(word, self.get_more_complex_syllable(syll))
+        self.lexicon[pos][word] = self.inventory.get(word)
+
+    def get_lexical_element(self, word, pos):
+        elem = self.inventory.get(word)
+        if not elem:
+            self.gen_lexical_element(word, pos)
+            elem = self.inventory.get(word)
+        return elem
+
+    def build_sentence(self, *words):
+        nom_elems = ['obj', 'subj']
+        elems = dict([(word[1], (self.get_lexical_element(word[0], word[1]) ,word[2])) for word in words])
+        struct = self.syntax_instance.inventory.get('sentence')  # e.g {subj}{verb}{obj}
+        parts = [info[0] for info in elems.values()]
+        if 'prep_verbal' in parts:
+            struct = struct.format(
+                verb=self.syntax_instance.inventory.get('adjunct_verbal').format(adjunct=),
+                                   object='{obj}', subj='{subj}')
+        if 'prep_subj' in parts:
+            struct = struct.format(verb='{verb}', obj='{obj}', subj=self.syntax_instance.inventory.get(
+                'adjunct_nominal').format(nominal='{subj}', adjunct='{adjunct}'))
+        if 'prep_obj' in parts:
+            struct = struct.format(verb='{verb}', subj='{subj}', obj=self.syntax_instance.inventory.get(
+                'adjunct_nominal').format(nominal='{obj}', adjunct='{adjunct}'))
+
+        if 'neg_verbal' in parts:
+            struct = struct.format(subj='{subj}', obj='{obj}', verb=self.other_instance.inventory.get('neg_vp').format(
+                neg_verbal=self.inventory.get('neg_verbal'), verb='{verb}'))
+        if 'neg_subj' in parts:
+            struct = struct.format(obj='{obj}', verb='{verb}', subj=self.other_instance.inventory.get('neg_np').format(
+                neg_nominal=self.other_instance.inventory.get('neg_nominal'), nominal='{subj}'))
+        if 'neg_obj' in parts:
+            struct = struct.format(subj='{subj}', verb='{verb}', obj=self.other_instance.inventory.get('neg_np').format(
+                neg_nominal=self.other_instance.inventory.get('neg_nominal'), nominal='{obj}'))
+        if 'poss_subj' in parts:
+            poss_phrase = self.other_instance.inventory.get('poss_phrase')
+            possessor = elems.pop('possessor')
+            possessor = self.nominal_instance.get_nominal(root=possessor[0], gender=possessor[1].get('gender'),
+                                                          number=possessor[1].get('number'),
+                                                          case=poss_phrase[1:4])
+            struct = struct.format(verb='{verb}', obj='{obj}',
+                                   subj=self.other_instance.inventory.get('poss_phrase').format(nominal='{subj}',
+                                                                                                acc_nominal=possessor,
+                                                                                                dat_nominal=possessor,
+                                                                                                gen_nominal=possessor))
+        if 'poss_obj' in parts:
+            poss_phrase = self.other_instance.inventory.get('poss_phrase')
+            possessor = elems.pop('possessor')
+            possessor = self.nominal_instance.get_nominal(root=possessor[0], gender=possessor[1].get('gender'),
+                                                          number=possessor[1].get('number'),
+                                                          case=poss_phrase[1:4])
+            struct = struct.format(verb='{verb}', subj='{subj}',
+                                   obj=self.other_instance.inventory.get('poss_phrase').format(nominal='{obj}',
+                                                                                                acc_nominal=possessor,
+                                                                                                dat_nominal=possessor,
+                                                                                                gen_nominal=possessor))
+        if 'indir_obj' in parts:
+            io_phrase = self.other_instance.inventory.get('indir_obj')
+            io = elems.pop('indir_obj')
+            if 'acc' in io_phrase:
+                case = 'acc'
+            elif 'dat' in io_phrase:
+                case = 'dat'
+            elif 'gen' in io_phrase:
+                case = 'gen'
+            else:
+                case = 'nom'
+            io = self.nominal_instance.get_nominal(root=io[0], gender=io[1].get('gender'), number=io[1].get('number'),
+                                                   case=case)
+            io_phrase = io_phrase.format(
+                prep1=self.other_instance.inventory.get('prep_io'),
+                prep2=self.other_instance.inventory.get('prep_io2', ''),
+                nominal=io)
+
+            l = struct.split('}')
+            l = [x + '}' for x in l if x]
+            l.insert(randint(0,len(l)), 'io_phrase')
+            struct = ' '.join(l)
+        for elem in elems:
--- morpheme_generator.py
+++ morpheme_generator.py
@@ -1,22 +1,13 @@
 from random import choice
 
-from methodselector import MethodSelector, StringRedis
-from phono import Phonology
-
-
-class MorphemeGeneratorMixin(MethodSelector):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.syllables = set()
-
+class MorphemeGeneratorMixin:
     def map_syll_to_structure(self, syll):
-        vowels = self.connection.get(Phonology.construct_key('V'))
         if len(syll) == 2:
             return 'CV'
         elif len(syll) == 4:
             return 'CVCV'
         else:
-            if syll[-1] in vowels:
+            if syll[-1] in self.inventory['V']:
                 return 'CVV'
             else:
                 return 'CVC'
@@ -25,7 +16,7 @@ class MorphemeGeneratorMixin(MethodSelector):
         if not syll:
             return 'CV'
         structure = self.map_syll_to_structure(syll)
-        hierarchy = self.config['hierarchy']
+        hierarchy = ['CV', 'CVC', 'CVV', 'CVCV']
         try:
             return hierarchy[hierarchy.index(structure) + 1]
         except IndexError:
@@ -33,22 +24,19 @@ class MorphemeGeneratorMixin(MethodSelector):
 
     def gen_morpheme(self, label, syll_structure='CV', noop=False):
         structure = syll_structure.capitalize()
+        templates = {'CV': '{c1}{v1}', 'CVC': '{c1}{v1}{c2}', 'CVV': '{c1}{v1}{v2}', 'CVCV': '{c1}{v1}{c2}{v2}'}
         if noop:
             syll = ''
         else:
             syll = None
-            template = self.config['templates'][structure]
-            max_syll = self.connection.get('max_{}'.format(structure))
-            vowels = self.connection.get(Phonology.construct_key('V'))
-            consonants = self.connection.get(Phonology.construct_key('C'))
+            template = templates[structure]
             while (not syll) or syll in self.syllables:
-                if len(self.syllables) < max_syll:
-                    syll = template.format(c1=choice(consonants),
-                                           v1=choice(vowels),
-                                           c2=choice(consonants),
-                                           v2=choice(vowels))
+                if len(self.syllables) < self.inventory['max_{}'.format(structure)]:
+                    syll = template.format(c1=choice(self.phonological_inventory['C']),
+                                           v1=choice(self.phonological_inventory['V']),
+                                           c2=choice(self.phonological_inventory['C']),
+                                           v2=choice(self.phonological_inventory['V']))
                 else:
                     template = self.get_more_complex_syllable(structure)
             self.syllables.add(syll)
-        self.connection.hset(self.construct_key('inventory'), label, syll)
         self.inventory[label] = syll
